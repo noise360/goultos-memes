c98bb1027495362b4e77323a5c0cb5fbbb3f709a
noise360 <121175451+noise360@users.noreply.github.com>
2022-12-22 11:58:36 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "arno.ishmam@mail.com";
 $subject = "Mail From akijian memes";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@github.io" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
